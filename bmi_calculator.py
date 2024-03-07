a1fda3b2cc913331079f9f6d64a1b8ba3f6eb9ad
SpeedyButOnGithub <tevmetuiwe2@yahoo.com>
2024-03-07 19:06:17 +0100

    copy paste bmi calculator

---
+++ bmi_calculator.py
@@ -0,0 +1,23 @@
+def calculate__bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
+
+def interpret_bmi(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 25:
+        return "Normal weight"
+    elif 25 <= bmi < 30:
+        return "Overweight"
+    else:
+        return "Obese"
+    
+if __name__ == "__main__":
+    weight = float(input("Enter your weight in kilograms: "))
+    height = float(input("Enter your height in meters: "))
+
+    bmi = calculate__bmi(weight, height)
+    category = interpret_bmi(bmi)
+
+    print("Your bmi is:", round(bmi, 2))
+    print("You are classified as:", category)
