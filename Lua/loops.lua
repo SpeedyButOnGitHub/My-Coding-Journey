ce8ba507c79f7da9177fe1672dd3a67417319d09
SpeedyButOnGithub <tevmetuiwe2@yahoo.com>
2024-10-07 17:08:01 +0200

    Checking out lua loops

---
+++ Lua/loops.lua
@@ -0,0 +1,22 @@
+local number = 1
+
+for i = 1, 5, 1 do
+    print(number)
+    number = number + 1
+end
+
+local number2 = 6
+
+while number2 < 11 do
+    print(number2)
+    number2 = number2 + 1
+end
+
+for i = 1, 5, 1 do
+    print("A")
+
+    for x = 1, 5, 1 do
+        print("B")
+    end
+end
+
