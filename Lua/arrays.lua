359abd851c269ea4715fefb58939a661451f9a30
SpeedyButOnGithub <tevmetuiwe2@yahoo.com>
2024-10-08 14:57:26 +0200

    Checking out arrays in lua

--- Lua/arrays.lua
+++ Lua/arrays.lua
@@ -15,3 +15,11 @@ table.remove(myArray, 4)
 for i = 1, #myArray do
     print(myArray[i])
 end
+
+local myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+for i = 1, #myArray do
+    if myArray[i] == 5 then
+        print("5 has been found.")
+    end
+end
