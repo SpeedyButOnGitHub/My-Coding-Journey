03cf9f4ade61024a596cf0d80c9d9d82e447644c
SpeedyButOnGithub <tevmetuiwe2@yahoo.com>
2024-10-08 15:04:06 +0200

    Dictionaries in lua

---
+++ Lua/dictionaries.lua
@@ -0,0 +1,17 @@
+local menu = {
+    ["Cheeseburger"] = 6,
+    ["Coca cola"] = 2,
+    ["French fries"] = 2,
+    ["Chicken nuggets"] = 2,
+}
+
+menu["Cheeseburger"] = 5
+
+print("$"..menu["Cheeseburger"])
+
+local menu2 = {
+    Cheeseburger = 6,
+    Coca_Cola = 2
+}
+
+print("$"..menu2.Cheeseburger)
